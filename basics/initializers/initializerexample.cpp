f0da1abcc8410d212b38c5dc86a02f198a239ee4
Dave Spina <dspina79@gmail.com>
2022-02-24 15:45:02 -0500

    Initializers

---
+++ basics/initializers/initializerexample.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int firstInt {19};
+    int secondInt {15};
+    int sumInt {firstInt + secondInt};
+
+    std::cout << "firstInt: " << firstInt << std::endl;
+    std::cout << "secondInt: " << secondInt << std::endl;
+    std::cout << "sumInt: " << sumInt << std::endl;
+    return 0;
+}
