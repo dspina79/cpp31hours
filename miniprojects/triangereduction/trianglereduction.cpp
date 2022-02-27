a9ec870519b761d01cba2c54662c0e28b3357b3a
Dave Spina <dspina79@gmail.com>
2022-02-27 10:35:22 -0500

    Finished first version of triangle reduction

--- miniprojects/triangereduction/trianglereduction.cpp
+++ miniprojects/triangereduction/trianglereduction.cpp
@@ -1,21 +1,49 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+#include <array>
 
 using namespace std;
 
+void printarray(int arr[], int arrSize) {
+    for (int i = 0; i < arrSize; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+}
+
 int main() {
     const int STARTER_BASE = 9;
+    int currentBase = STARTER_BASE;
     const int MAX_NUM = 10;
+    int collection[STARTER_BASE];
 
     srand(time(nullptr));
 
     for (int i = 0; i < STARTER_BASE; i++) {
         int x = rand()/((RAND_MAX + 1u)/MAX_NUM);
-        cout << x << " ";
+        // found that the first number was always 0
+        if (i == 0 && x == 0) {
+            x = rand()/((RAND_MAX + 1u)/MAX_NUM);
+        }
+        collection[i] = x;
     }
 
-    cout << endl;
-
+    printarray(collection, STARTER_BASE);
+    
+    while(currentBase > 1) {
+        int tempCollection[currentBase--];
+        for (int i = 0; i < currentBase; i++) {
+            tempCollection[i] = abs(collection[i] - collection[i+1]);
+        }
+        
+        for (int i = 0; i < currentBase; i++) {
+            collection[i] = tempCollection[i];
+        }
+        
+        printarray(collection, currentBase);
+    }
+    
     return 0;
 }
