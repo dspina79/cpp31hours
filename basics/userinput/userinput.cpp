e7010ba88a0b5f3118130d855f0f6d2ef6cfbe16
Dave Spina <dspina79@gmail.com>
2022-02-27 10:00:38 -0500

    Added example of user input

---
+++ basics/userinput/userinput.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
+    cout << "Hello, " << name << endl;
+
+    return 0;
+}
