8670e9efe4f188bc433bdce22e5bd42c721bc0ad
Dave Spina <dspina79@gmail.com>
2022-02-28 09:14:13 -0500

    Added basic control flows

---
+++ basics/controlflow/basiccontrolflow.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 50;
+    string name = "Dean";
+    char person[7] = {'H', 'a', 'r', 'r', 'i', 'e', 't'};
+
+    // conditions
+
+    if (x >= y) {
+        cout << "x is greater than or equal to y" << endl;
+    } else {
+        cout << "x is less than y" << endl;
+    }
+
+    // for loops
+    for (int i = 0; i < x; i++) {
+        cout << (i + 1) << " ";
+    }
+    cout << endl;
+
+    for (int j = 0; j < 7; j++) {
+        cout << person[j];
+    }
+    cout << endl;
+
+    // while loops
+    int v = 13;
+    while (v > x) {
+        cout << "The value of v is: " << v << endl;
+        v--;
+    }
+
+    // do/while
+    do {
+        cout << "This will print out no matter what..." << endl;
+    } while(false);
+}
