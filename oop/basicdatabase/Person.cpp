c54e9c1e9facd6dba320d717fb411a92dd30be14
Dave Spina <dspina79@gmail.com>
2022-03-13 19:14:16 -0400

    Finished address

--- oop/basicdatabase/Person.cpp
+++ oop/basicdatabase/Person.cpp
@@ -24,10 +24,56 @@ class Address {
         void setStateProvince(string stateProvince) { _state = stateProvince; }
         void setPostalCode(string postalCode) { _postalCode = postalCode; }
         void setCountryCode(string countryCode) { _countryCode = countryCode; }
+        void printAddress();
         Address();
         Address(string, string, string, string);
-        Address(string, string, string, string, string);
+        Address(string, string, string, string, string, string);
 };
+
+Address::Address() {
+    this->_address1 = "";
+    this->_address2 = "";
+    this->_city = "";
+    this->_state = "";
+    this->_postalCode = "";
+    this->_countryCode = "";
+}
+
+Address::Address(string address1, string city, string stateProvince, string postalCode) {
+    this->_address1 = address1;
+    this->_address2 = "";
+    this->_city = city;
+    this->_state = stateProvince;
+    this->_postalCode = postalCode;
+    this->_countryCode = "";
+};
+
+Address::Address(string address1, string address2, string city, string stateProvince, string postalCode, string countryCode) {
+    this->_address1 = address1;
+    this->_address2 = address2;
+    this->_city = city;
+    this->_state = stateProvince;
+    this->_postalCode = postalCode;
+    this->_countryCode = countryCode;
+}
+
+void Address::printAddress() {
+    string delim = ", ";
+    cout << this->_address1 << endl;
+
+    if (this->_address2 != "") {
+        cout << this->_address2 << endl;
+    }
+    if (this->_city == "" || this->_state == "") {
+        delim = " ";
+    }
+
+    cout << this->_city << delim << this->_state << "  " << this->_postalCode << endl;
+    
+    if (this->_countryCode != "") {
+        cout << this->_address1 << endl;
+    }
+}
 
 class Person {
     private:
@@ -35,5 +81,5 @@ class Person {
         string _lastName;
         string _middleName;
         string _dob;
-        
+        Address *addr;
 };
